a05b7fe0717f292bccd0c63ba32830750038e8d6
Jeff Zapotoczny <jeff.zapotoczny@sonarsource.com>
2020-09-20 09:01:19 -0400

    Updating SCM scripts

--- github.sh
+++ github.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+git remote remove origin
 git remote add origin https://github.com/jeff-zapotoczny-sonarsource/forrester-sast-wave.git
 git branch -M master
 git push -u origin master
--- gitlab.sh
+++ gitlab.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+git remote remove origin
 git remote add origin https://gitlab.com/jeff.zapotoczny/forrester-sast-wave.git
-#git branch -M master
-#git push -u origin master
+git branch -M master
+git push -u origin master
