a4ba527871f57ece3716ce899e445acb5a59ca35
Jeff Zapotoczny <jeff.zapotoczny@sonarsource.com>
2020-09-20 09:39:14 -0400

    Adding another hotspot

--- src/main/java/demo/security/Utils.java
+++ src/main/java/demo/security/Utils.java
@@ -3,18 +3,25 @@ package demo.security;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
+import java.util.Random;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 
 public class Utils {
 
+    private Random rando;
+
+    public Utils() {
+        this.rando = new Random();
+    }
+
     public void modResponse(HttpServletResponse response) {
         Cookie c = new Cookie("SECRET", "SECRET");
         response.addCookie(c);
     }
 
-    public KeyPair weakKey() {
+    public KeyPair makeKey() {
         KeyPairGenerator keyPairGen;
         try {
             keyPairGen = KeyPairGenerator.getInstance("RSA");
@@ -23,5 +30,11 @@ public class Utils {
         } catch (NoSuchAlgorithmException e) {
             return null;
         }
+    }
+
+    public byte[] generateRandomBytes() {
+        byte[] bytes = new byte[20];
+        rando.nextBytes(bytes); 
+        return bytes;
     }
 }
