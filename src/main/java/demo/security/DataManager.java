f255dba476b00bacddc023380b724dfd215d1e46
Jeff Zapotoczny <jeff.zapotoczny@sonarsource.com>
2020-09-20 09:57:47 -0400

    Commenting out hotspots

--- src/main/java/demo/security/DataManager.java
+++ src/main/java/demo/security/DataManager.java
@@ -13,20 +13,20 @@ public class DataManager {
         framework = new ProprietaryFramework();
     }
 
-    public String getUser(Connection connection, String user) throws SQLException {
-        /* try (Statement statement = connection.createStatement();
+/*     public String getUser(Connection connection, String user) throws SQLException {
+        try (Statement statement = connection.createStatement();
                 ResultSet resultSet = statement
                         .executeQuery("SELECT userid FROM users WHERE username = '" + user + "'");) {
             return resultSet.getString(1);
-        } */
-    }
+        }
+    } */
 
-    public String getThing(Connection connection, String param) throws SQLException {
-        /* try (Statement statement = connection.createStatement();
+/*     public String getThing(Connection connection, String param) throws SQLException {
+        try (Statement statement = connection.createStatement();
                 ResultSet rs = statement.executeQuery("select thing from others WHERE param=" + param);) {
             return rs.getString(1);
-        } */
-    }
+        }
+    } */
 
     public void storeData(String data) {
         framework.storeData(data);
